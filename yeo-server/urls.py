6e2f3bb76242a7bcb894942743fd1be10e78dd32
Eugene <yeo.eugene.oey@gmail.com>
2011-08-19 18:13:11 +0800

    enable django admin

--- yeo-server/urls.py
+++ yeo-server/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    (r'^admin/', include(admin.site.urls)),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
 )
